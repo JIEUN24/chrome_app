45d7c95ae15cf836941c01511fdf218ecbc0fc6d
JIEUNNN <pandaa0628@gmail.com>
2022-02-03 22:00:21 +0900

    second commit

--- js/todo.js
+++ js/todo.js
@@ -2,15 +2,26 @@ const toDoForm = document.getElementById("todo-form")
 const toDoInput = document.querySelector("#todo-form input")
 const toDoList = document.getElementById("todo-list")
 
+const TODOS_KEY = "todos"
+
+let toDos = []
+
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(toDos))
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement
     li.remove()
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id))
+    saveToDos()
 }
 
 function paintToDo(newTodo) {
     const li = document.createElement("li")
+    li.id = newTodo.id
     const span = document.createElement("span")
-    span.innerText = newTodo
+    span.innerText = newTodo.text
     const button = document.createElement("button")
     button.innerText = "x"
     button.addEventListener("click", deleteToDo)
@@ -23,6 +34,27 @@ function handleToDoSubvit(event) {
     event.preventDefault()
     const newTodo = toDoInput.value
     toDoInput.value = ""
-    paintToDo(newTodo)
+    const newTodoObj = {
+        text: newTodo,
+        id : Date.now(),
+    }
+    toDos.push(newTodoObj)
+    paintToDo(newTodoObj)
+    saveToDos()
 }
+
 toDoForm.addEventListener("submit", handleToDoSubvit)
+
+// function sayHello(item) {
+//     console.log("this is the turn of", item)
+// }
+
+const savedToDos = localStorage.getItem(TODOS_KEY)
+
+if(savedToDos !== null){
+    const parsedTodos = JSON.parse(savedToDos)
+    toDos = parsedTodos
+    parsedTodos.forEach(paintToDo);
+    // sayHello 함수를 따로 만들지 않고 더 짧게 쓰는 방법(arrow function)
+    // parsedTodos.forEach((item) => console.log("this is the turn of", item));
+}
---
+++ js/weather.js
@@ -0,0 +1,23 @@
+
+const API_KEY = "b1b329422f786e2986f06f07df42ba51"
+
+// 유저의 위치를 파악하는 정보
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude
+    const lng = position.coords.longitude
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+            const weather = document.querySelector("#weather span:first-child")
+            const city = document.querySelector("#weather span:last-child")
+            city.innerText = data.name
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
+        })
+    }
+function onGeoError() {
+    alert("Can't find you. No weather for you")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError)
